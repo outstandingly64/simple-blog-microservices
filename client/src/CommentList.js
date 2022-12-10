e703ac749c9b2d60a4867e6d26ead66e149b936a
Ivan Tello <ivantello9922@gmail.com>
2022-12-10 18:30:59 -0500

    moderation effects in React App

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,7 +3,21 @@ import React from 'react';
 const CommentList = ({ comments }) => {
 
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+      let content;
+
+      if(comment.status === 'accepted'){
+        content = comment.content;
+      }
+
+      if(comment.status === 'rejected'){
+        content = 'This comment has been rejected';
+      }
+
+      if(comment.status === 'pending'){
+        content = 'This comment is pending moderation';
+      }
+
+        return <li key={comment.id}>{content}</li>
     });
 
   return (
